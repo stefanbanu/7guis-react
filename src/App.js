b916c5a795481c37bd90a6216d07dc8b37ee4a20
stefanbanu <stefanbanu1@gmail.com>
2020-03-29 07:55:46 +0300

    starting new project 'timer'

--- src/App.js
+++ src/App.js
@@ -2,8 +2,41 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { actions, reducer, initialState, flightTypes } from "./state";
+import { Link, BrowserRouter, Switch, Route } from "react-router-dom";
+import Timer from "./timer/timer";
 
 function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/flight-booker">Flight Booker</Link>
+          </li>
+          <li>
+            <Link to="/timer">Timer</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/flight-booker">
+          <FlightBooker></FlightBooker>
+        </Route>
+        <Route exact path="/">
+          <h1>Home Page</h1>
+        </Route>
+        <Route path="/timer">
+          <Timer></Timer>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+function FlightBooker() {
   const [state, dispatch] = React.useReducer(reducer, initialState);
 
   let isFormValid;
---
+++ src/timer/timer.js
@@ -0,0 +1,8 @@
+import React from "react";
+// 1.state
+// 2. hooking up to react
+function Timer() {
+  return <h1>Hello from Timer</h1>;
+}
+
+export default Timer;
